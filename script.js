4822f2e33467c95fbbac22fdfd48b8ed04692a97
gabrielTesti <gabrieltesti22@gmail.com>
2024-06-27 22:12:38 -0300

    27/6

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const eliminarTodo = document.querySelector(".eliminarTodo");
 eliminarTodo.style.display="none";
 
 
+//Cargar las tareas guardadas en el localStorage 
+document.addEventListener("DOMContentLoaded",loadTasks);
+
+
+
+
+
 addBtn.addEventListener("click", (e)=>{
     e.preventDefault();
     const text = input.value;
@@ -24,14 +31,18 @@ addBtn.addEventListener("click", (e)=>{
     input.value="";
     empty.style.display="none";
     eliminarTodo.style.display="block";
-   
+
+    saveTasks();
     }
+    input.focus();
 });
 
 eliminarTodo.addEventListener("click", (e)=>{
     ul.innerHTML="";
     eliminarTodo.style.display="none";
     empty.style.display="block"
+    localStorage.removeItem("tasks")
+    input.focus();
 })
 
 function addDeleteBtn(){
@@ -47,10 +58,37 @@ function addDeleteBtn(){
     if(items.length===0){
         empty.style.display="block";
         eliminarTodo.style.display="none";
-        
     }
+    saveTasks();
+    input.focus();
  });
  return deleteBtn;
+}
+
+
+function saveTasks(){
+    const tasks = [];
+    const items = document.querySelectorAll("li p");
+    items.forEach(item=>tasks.push(item.textContent));
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+function loadTasks(){
+    const tasks = JSON.parse(localStorage.getItem("tasks"))||[]; /*se utiliza para proporcionar un valor por defecto en caso de que localStorage.getItem('tasks') devuelva null*/
+    tasks.forEach(task=>{
+        const li = document.createElement("li");
+        const p = document.createElement("p")
+        p.textContent=task;
+        li.appendChild(p);
+        li.appendChild(addDeleteBtn());
+        ul.appendChild(li);
+    })
+
+    if(tasks.length>0){
+        empty.style.display="none";
+        eliminarTodo.style.display="block"
+    }
+    input.focus();
 }
 
 
